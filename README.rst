32e655458815230a8914e41ceea69f04877e8a23
jepayne1138 <jepayne1138@gmail.com>
2016-07-12 11:39:13 -0500

    Initial commit. Project skeleton and README, LICENCE and setup files.

---
+++ README.rst
@@ -0,0 +1,16 @@
+ModularMailer-PluginBase
+========================
+
+Description
+-----------
+Defines abstract base classes that all plugins for the ModularMailer project should subclass from. Currently this will define only the requirements for creating a table import driver plugin (a plugin that allows a specific file type to be read and imported into the application).
+
+If the future, it is planned that more plugins will be defined for different purposes in the ModularMailer application, and this package will be update accordingly as these are defined.
+
+Installation
+------------
+Installation should be as simple as installing from PyPI using pip::
+
+    pip install ModularMailer-PluginBase
+
+There are currently no requirements or dependencies for this package.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='ModularMailer-PluginBase',
+    packages=['plugin_base'],
+    version='0.0a1',
+    description='ModularMailer plugin base class definition',
+    author='James Payne',
+    author_email='jepayne1138@gmail.com',
+    url='https://github.com/jepayne1138/ModularMailer-PluginBase',
+    license='BSD-new',
+    download_url='https://github.com/jepayne1138/ModularMailer-PluginBase/tarball/0.0a1',
+    keywords='plugin abc',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Plugins',
+    ],
+)
